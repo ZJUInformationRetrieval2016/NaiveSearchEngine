b0b6ac96d84f76e3da23e6b14935e6a32b7db4b0
c34 <vincent007x@outlook.com>
2016-06-22 13:22:54 +0800

    final

--- project/main.py
+++ project/main.py
@@ -8,17 +8,18 @@ import ScoreSort
 import Boolsearch
 
 if (__name__ == '__main__'):
+    K = 16
     while (1):
         _input = ''
         _input = input('# ')
         if (_input.endswith('-b')):
-            ret = Boolsearch.search(_input[:-2], 16)
+            ret = Boolsearch.search(_input[:-2], K)
             for i in ret:
                 print(i)
         else:
             tokens = Index.parse(_input)
             print("Search: ", tokens)
             if tokens:
-                ret = ScoreSort.sort(tokens, 16)
+                ret = ScoreSort.sort(tokens, K)
             for i in ret:
                 print(i)
